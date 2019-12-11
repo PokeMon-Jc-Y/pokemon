c8e23379f864e0ca2d38bdaab2e5ec57bcd90841
Rentigger <58659720+Rentigger@users.noreply.github.com>
2019-12-12 02:12:19 +0800

    STEP3

--- pokemon/01.py
+++ pokemon/01.py
@@ -5,9 +5,20 @@ from pygame.locals import *
 
 screen=pygame.display.set_mode([900,600])
 Map = []
+Dynamicstate_point = 0
+press = False
 
-Grass_ground = c.terrain.Terrain(pygame.image.load('image/grass.jpg').convert_alpha(),"ground")
-Grass_ground1 = c.terrain.Terrain(pygame.image.load('image/grass1.jpg').convert_alpha(),"ground")
+#动态滚动时坐标轴方向偏移量符号
+x=0
+y=0
+
+Grass_ground = c.terrain.Terrain([pygame.image.load('image/grass.jpg').convert_alpha()],"ground")
+Grass_ground1 = c.terrain.Terrain([pygame.image.load('image/grass1.jpg').convert_alpha()],"ground")
+flower = c.terrain.Terrain(
+    [pygame.image.load('image/flower0.jpg').convert_alpha(),
+     pygame.image.load('image/flower1.jpg').convert_alpha(),
+     pygame.image.load('image/flower2.jpg').convert_alpha()],"flower")
+
 
 player = c.character.Character('player',
 [
@@ -28,13 +39,26 @@ pygame.image.load('image/player/right2.png').convert_alpha()]
 
 Tree = pygame.image.load('image/tree.png').convert_alpha()
 
-for i in range(40):
+for i in range(42):
     row=[]
-    for j in range(40):
+    for k in range(i):
+        row.append([Grass_ground1,''])
+    for j in range(i,42):
         row.append([Grass_ground,''])
     Map.append(row)
 
+Map[10][11] = [flower,'']
+Map[10][12] = [flower,'']
+Map[11][11] = [flower,'']
+Map[11][12] = [flower,'']
+Map[12][11] = [flower,'']
+Map[12][12] = [flower,'']
+
 Player_location = [10,10]
+
+
+
+
 
 def add_tree(location):
     Map[location[0]][location[1]-1][1] = 'tree_image'
@@ -43,7 +67,8 @@ def add_tree(location):
     Map[location[0]+1][location[1]][1] = 'tree'
     Map[location[0]+1][location[1]+1][1] = 'tree'
 
-for i in range(0,39,2):
+
+for i in range(0,41,2):
     for j in range(-1,39,2):
         if j<7 or j >29 or i<8 or i>31:
             add_tree([i,j])
@@ -55,41 +80,12 @@ def refresh():
     global screen
     global Grass_ground
     global Player_location
-    pianyi = 6*player.Clock
-    if player.Direction == 0 or player.Direction == 1:
-        y = (player.Direction-0.5)*2
-        x = 0
-    else:
-        y = 0
-        x = (player.Direction-2.5)*2
+    global x
+    global y
+    global Dynamicstate_point
+    global press
 
-    for i in range(11):
-        for j in range(0,15):
-            screen.blit((Map[Player_location[0]-7+j][Player_location[1]-5+i][0]).Image,[j*60+x*pianyi,i*60+y*pianyi])
-
-    for i in range(-1,15):
-        for j in range(-1,15):
-            if  Map[Player_location[0]-7+j][Player_location[1]-5+i][1] == 'tree_image':
-                screen.blit(Tree,[j*60+x*pianyi,(i-1)*60+y*pianyi])
-
-    if player.Clock ==0 :
-        screen.blit(player.Image[player.Direction][0],[7*60,4*60])
-    else:
-        player.Clock -=1
-        screen.blit(player.Image[player.Direction][1+player.Point%2],[7*60,4*60])
-        if player.Clock == 0:
-            player.Point +=1
-    
-
-while 1: 
-    refresh() 
-
-    pygame.display.flip()
-
-    pygame.time.delay(20)
-
-    
-    
+    Dynamicstate_point +=1
 
 
     for event in pygame.event.get():
@@ -98,29 +94,125 @@ while 1:
             exit(0)
         if event.type==pygame.KEYDOWN and player.Clock == 0:
             if event.key==K_w:
-                if player.Direction == 1 and Player_location[1]>8:
+                press = True
+                if player.Direction == 1 and Player_location[1]>6:
                     Player_location[1]-=1
+                    y = -1
+                    x = 0
                 else:
-                    player.Direction = 1
-                player.Clock = 10
+                    player.Direction = 1    
+                player.Clock = 9
             elif event.key==K_a:
+                press = True
                 if player.Direction == 2 and Player_location[0]>8:
                     Player_location[0]-=1
+                    y = 0
+                    x = -1
                 else:
                     player.Direction = 2
-                player.Clock = 10
+                player.Clock = 9
             elif event.key==K_s:
-                if player.Direction == 0 and 31>Player_location[1]:
+                press = True
+                if player.Direction == 0 and 29>Player_location[1]:
                     Player_location[1]+=1
+                    y = 1
+                    x = 0
                 else:
                     player.Direction = 0
-                player.Clock = 10
+                player.Clock = 9
             elif event.key==K_d:
+                press = True
                 if player.Direction == 3 and 31>Player_location[0]:
                     Player_location[0]+=1
+                    y = 0
+                    x = 1
                 else:
                     player.Direction = 3
-                player.Clock = 10
+                player.Clock = 9
+
+
+        if event.type==pygame.KEYUP:
+                if event.key==pygame.K_w:
+                    press=False                 
+                elif event.key==pygame.K_a:
+                    press=False
+                elif event.key==pygame.K_s:
+                    press=False
+                elif event.key==pygame.K_d:
+                    press=False
+
+
+    
+    #连续方向键前进
+    if press == True and player.Clock == 0:
+        player.Clock = 9
+
+        if player.Direction == 1 and Player_location[1]>6:
+            Player_location[1]-=1
+            y = -1
+            x = 0
+ 
+        if player.Direction == 2 and Player_location[0]>8:
+            Player_location[0]-=1
+            y = 0
+            x = -1
+
+        if player.Direction == 0 and 29>Player_location[1]:
+            Player_location[1]+=1
+            y = 1
+            x = 0
+
+        if player.Direction == 3 and 31>Player_location[0]:
+            Player_location[0]+=1
+            y = 0
+            x = 1
+
+    pianyi = 6*player.Clock
+    #加载草插图
+    for i in range(11):
+        for j in range(-2,17):
+            if len((Map[Player_location[0]-7+j][Player_location[1]-5+i][0]).Image)==1 :
+                screen.blit((Map[Player_location[0]-7+j][Player_location[1]-5+i][0]).Image[0],[j*60+x*pianyi,i*60+y*pianyi])
+            else:
+                #print((Dynamicstate_point%4+1)%2+2*((Dynamicstate_point%4+1)//4))
+                screen.blit((Map[Player_location[0]-7+j][Player_location[1]-5+i][0]).Image[(Dynamicstate_point%36//9+1)%2+2*((Dynamicstate_point%36//9+1)//4)],[j*60+x*pianyi,i*60+y*pianyi])
+
+
+    for i in range(-2,17):
+        for j in range(-2,17):
+            #加载人物插图
+            if i==7 and j==4:
+                if player.Clock ==0 :
+                    screen.blit(player.Image[player.Direction][0],[7*60,4*60])
+                    x=0
+                    y=0
+                else:
+                    player.Clock -=1
+                    screen.blit(player.Image[player.Direction][1+player.Point%2],[7*60,4*60])
+                    if player.Clock == 0:
+                        player.Point +=1
+                  
+            #加载边界树插图
+            if  Map[Player_location[0]-7+j][Player_location[1]-5+i][1] == 'tree_image':
+                screen.blit(Tree,[j*60+x*pianyi,(i-1)*60+y*pianyi])
+
+    
+
+    
+    
+
+while 1: 
+    refresh() 
+
+    pygame.display.flip()
+
+    pygame.time.delay(30)
+
+    
+    
+
+
+    
 
 
 
