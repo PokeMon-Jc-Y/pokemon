f9fd132d72532b4944c65e2ef4cad95243925d2b
Rentigger <58659720+Rentigger@users.noreply.github.com>
2019-12-16 17:38:32 +0800

    debug（连续行走撞墙鬼畜）

--- pokemon/01.py
+++ pokemon/01.py
@@ -39,11 +39,11 @@ pygame.image.load('image/player/right2.png').convert_alpha()]
 
 Tree = pygame.image.load('image/tree.png').convert_alpha()
 
-for i in range(42):
+for i in range(40):
     row=[]
     for k in range(i):
         row.append([Grass_ground1,''])
-    for j in range(i,42):
+    for j in range(i,40):
         row.append([Grass_ground,''])
     Map.append(row)
 
@@ -65,7 +65,7 @@ def add_tree(location):
     Map[location[0]+1][location[1]+1][1] = 'tree'
 
 
-for i in range(0,41,2):
+for i in range(0,39,2):
     for j in range(-1,39,2):
         if j<7 or j >29 or i<8 or i>31:
             add_tree([i,j])
@@ -146,40 +146,42 @@ def refresh():
 
 
     
-    #Continuous arrow keys go forward
+    #continuous moving
     if press != '' and player.Clock == 0:
-        player.Clock = 9
 
         if player.Direction == 1 and Player_location[1]>6:
             Player_location[1]-=1
+            player.Clock = 9
             y = -1
             x = 0
  
         if player.Direction == 2 and Player_location[0]>8:
             Player_location[0]-=1
+            player.Clock = 9
             y = 0
             x = -1
 
         if player.Direction == 0 and 29>Player_location[1]:
             Player_location[1]+=1
+            player.Clock = 9
             y = 1
             x = 0
 
         if player.Direction == 3 and 31>Player_location[0]:
             Player_location[0]+=1
+            player.Clock = 9
             y = 0
             x = 1
 
-    pianyi = 6*player.Clock
-    print(player.Clock)
+    offset = 6*player.Clock
     #Loading straw illustration
     for i in range(11):
         for j in range(-2,17):
             if len((Map[Player_location[0]-7+j][Player_location[1]-5+i][0]).Image)==1 :
-                screen.blit((Map[Player_location[0]-7+j][Player_location[1]-5+i][0]).Image[0],[j*60+x*pianyi,i*60+y*pianyi])
+                screen.blit((Map[Player_location[0]-7+j][Player_location[1]-5+i][0]).Image[0],[j*60+x*offset,i*60+y*offset])
             else:
                 #print((Dynamicstate_point%4+1)%2+2*((Dynamicstate_point%4+1)//4))
-                screen.blit((Map[Player_location[0]-7+j][Player_location[1]-5+i][0]).Image[(Dynamicstate_point%36//9+1)%2+2*((Dynamicstate_point%36//9+1)//4)],[j*60+x*pianyi,i*60+y*pianyi])
+                screen.blit((Map[Player_location[0]-7+j][Player_location[1]-5+i][0]).Image[(Dynamicstate_point%36//9+1)%2+2*((Dynamicstate_point%36//9+1)//4)],[j*60+x*offset,i*60+y*offset])
 
 
     for i in range(-2,17):
@@ -198,7 +200,7 @@ def refresh():
                   
             #Loading tree illustration
             if  Map[Player_location[0]-7+j][Player_location[1]-5+i][1] == 'tree_image':
-                screen.blit(Tree,[j*60+x*pianyi,(i-1)*60+y*pianyi])
+                screen.blit(Tree,[j*60+x*offset,(i-1)*60+y*offset])
 
     
     
